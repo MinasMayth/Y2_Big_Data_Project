55c08779212a5aea9d97d38bdbada402cbb4b144
MinasMayth <minasmayth@protonmail.com>
2021-02-22 12:48:18 +0100

    Added kmeans scattering and labels to the points in the graph

--- data_collector.py
+++ data_collector.py
@@ -36,10 +36,10 @@ print(global_data.head())
 for date in range (30,31):
     current_clustering_data = global_data.loc[(global_data.date == '2021-01-'+str(date))]
     
-    print(current_clustering_data)
-    
     country_names = []
     country_ratios = []
+    x_axis = []
+    y_axis = []
     
     continents = ['North America','South America','Europe','Africa','Asia','World', 'Oceania']
     
@@ -47,39 +47,48 @@ for date in range (30,31):
     for i, country in current_clustering_data.iterrows():
         if country['location'] not in continents:
             local_ratio = country['total_tests']/country['population']
-            print("Ratio:",local_ratio)
             country_names.append(country['location'])
             country_ratios.append(local_ratio)
-        
-            countrys_tup = list(zip(country_names, country_ratios))
             
-            country_tup_filtered = [value for value in countrys_tup if math.isnan(value[1]) == False]
+            x_axis.append(country['population'])
+            y_axis.append(country['total_tests'])
+        
     
     
-    x_axis = [i for i in current_clustering_data['population'] if i not in continents]
-    y_axis = [i for i in current_clustering_data['total_tests'] if i not in continents]
-    
+    countrys_tup = list(zip(country_names, country_ratios))
     points = list(zip(x_axis,y_axis))
     
-    points_filtered = [value for value in points if math.isnan(value[1]) == False]
+    country_tup_filtered = [value for value in countrys_tup if math.isnan(value[1]) == False]
+    points_filtered = np.array([value for value in points if math.isnan(value[1]) == False])
+    
+    
     
     xs = [x[0] for x in points_filtered]
     ys = [x[1] for x in points_filtered]
     
     plt.scatter(xs, ys)
+    for coordinate, label in zip(points_filtered, [country[0] for country in country_tup_filtered]):
+        plt.annotate(xy=coordinate, text=label)
+        
     plt.show()
+
     
+
 # create kmeans object
-kmeans = KMeans(n_clusters=2)
+kmeans = KMeans(n_clusters=4)
 # fit kmeans object to data
 kmeans.fit(points_filtered)
 # print location of clusters learned by kmeans object
 print(kmeans.cluster_centers_)
 # save new clusters for chart
-y_km = kmeans.fit_predict(points_filtered)
+y_km = kmeans.fit_predict(points_filtered)   
 
 plt.scatter(points_filtered[y_km ==0,0], points_filtered[y_km == 0,1], s=100, c='red')
 plt.scatter(points_filtered[y_km ==1,0], points_filtered[y_km == 1,1], s=100, c='black')
 plt.scatter(points_filtered[y_km ==2,0], points_filtered[y_km == 2,1], s=100, c='blue')
 plt.scatter(points_filtered[y_km ==3,0], points_filtered[y_km == 3,1], s=100, c='cyan')
+
+for coordinate, label in zip(points_filtered, [country[0] for country in country_tup_filtered]):
+    plt.annotate(xy=coordinate, text=label)
+
 plt.show()
