ca05fe24d04e66795ed651465341b25d845f3d57
MinasMayth <minasmayth@protonmail.com>
2021-03-09 14:25:50 +0100

    US plots are done, we now have to move over to machine learning with europe

--- India/Indian_Test_Per_State.py
+++ India/Indian_Test_Per_State.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 #reading the document
-india_d_test = pd.read_csv(r'Project Data/India.csv', error_bad_lines=False, infer_datetime_format=True, index_col=False )
+india_d_test = pd.read_csv(r'../Project Data/India.csv', error_bad_lines=False, infer_datetime_format=True, index_col=False)
 
 #Removing the 'Other' Column from the document
 keep_col = ['Date','State','District','Confirmed','Recovered',"Deceased","Tested"]
@@ -15,7 +15,7 @@ CleanedData = pd.read_csv(r'india_Test_Cleaned.csv', error_bad_lines=False, infe
 #Dropping all the rows with a N/A value and creating a new csv file
 Ind = CleanedData.dropna(axis = 0, how = 'any')
 Ind.to_csv("FinalIndiaData", index=False)
-FData = pd.read_csv(r'FinalIndiaData', error_bad_lines=False, infer_datetime_format=True,)
+FData = pd.read_csv(r'../FinalIndiaData', error_bad_lines=False, infer_datetime_format=True, )
 
 #2021 - 02 - 11 is the most reccuring date
 
