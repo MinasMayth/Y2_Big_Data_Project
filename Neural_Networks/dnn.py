f533616998f813b00dda4d57af9327b0798818a3
ubervelocity <mihaitu.popescu@gmail.com>
2021-04-26 14:01:16 +0200

    Adds pipelines

--- Neural_Networks/dnn.py
+++ Neural_Networks/dnn.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras import layers
 from tensorflow.keras import Sequential
+from tensorflow_addons.metrics import RSquare
 
 
 def load_data(folder, filename):
@@ -34,8 +35,8 @@ def plot_loss(history):
 
 def build_and_compile_model(data):
     model = Sequential([
-        layers.Dense(64, activation='relu', input_shape=data.shape),
-        layers.Dense(64, activation='relu'),
+        layers.Dense(128, activation='relu', input_shape=data.shape),
+        layers.Dense(256, activation='relu'),
         layers.Dense(1)
     ])
 
@@ -43,13 +44,10 @@ def build_and_compile_model(data):
     return model
 
 
-if __name__ == '__main__':
-    # Load data
-    data = load_data('Project Data', 'US States Data.csv')
+def preprocess_data(data, features):
     for col in data.columns:
         print(col)
-    data = data[['Population (discrete data)', 'Tests (discrete data)', 'Gini - gov 2019 (continuous data)',
-                 '% urban population (continuous data)', 'Actual cases (measured) (discrete data)']]
+    data = data[features]
 
     # One-hot encode categorical variables
     # data = pd.get_dummies(data)
@@ -61,10 +59,12 @@ if __name__ == '__main__':
     data = pd.DataFrame(x_scaled)
 
     # Clean data by dropping NA rows
-    data = data.dropna()
+    return data.dropna()
 
+
+def split_data(data):
     # Train - Test split
-    train_dataset = data.sample(frac=0.8, random_state=0)
+    train_dataset = data.sample(frac=0.6, random_state=0)
     test_dataset = data.drop(train_dataset.index)
 
     # Look at ranges of features
@@ -77,21 +77,126 @@ if __name__ == '__main__':
     train_labels = train_features.pop(4)
     test_labels = test_features.pop(4)
 
-    print(train_features.shape)
+    return train_features, train_labels, test_features, test_labels
 
-    # Define model
-    model = build_and_compile_model(train_features)
-    model.summary()
 
+def train_model(model, train_features, train_labels):
     # Train model on data
     history = model.fit(
         train_features, train_labels,
         validation_split=0.2,
         verbose=1, epochs=100)
 
+    return model, history
+
+
+def plot_predictions(predictions, test_labels):
+    # Plot predictions
+
+    a = plt.axes(aspect='equal')
+    plt.scatter(test_labels, predictions)
+    plt.xlabel('True Values [Infections]')
+    plt.ylabel('Predictions [Infections]')
+    lims = [0, 1]
+    plt.xlim(lims)
+    plt.ylim(lims)
+    _ = plt.plot(lims, lims)
+    plt.show()
+
+
+def predict_infections_rsquare(model, test_features, test_labels):
+    # Predict values of test data and compute R-Squared coefficient
+    predicted = model.predict(test_features)
+    predicted = predicted.flatten()
+
+    # Measure the RSquare coefficient
+    metric = RSquare()
+    metric.update_state(test_labels, predicted)
+    result = metric.result()
+
+    # Return result
+    return result.numpy()
+
+
+def train_usa_test_usa():
+    # Load data
+    data = load_data('Project Data', 'US States Data.csv')
+
+    # Select some features to train and test on
+    features = ['Population (discrete data)', 'Tests (discrete data)', 'Gini - gov 2019 (continuous data)',
+                 '% urban population (continuous data)', 'Actual cases (measured) (discrete data)']
+
+    # Select features and clean data
+    data = preprocess_data(data, features)
+
+    # Split data into train and test samples
+    train_features, train_labels, test_features, test_labels = split_data(data)
+
+    # Define model
+    model = build_and_compile_model(train_features)
+    model.summary()
+
+    # Train model and retrieve performance of model during training
+    model, history = train_model(model, train_features, train_labels)
+
     # Plot loss during training history
     plot_loss(history)
 
     # Evaluate model on test data
-    dnn_results = model.evaluate(test_features, test_labels, verbose=0)
-    print(f'DNN Validation Loss: {dnn_results}')
+    dnn_results_usa = model.evaluate(test_features, test_labels, verbose=0)
+    print(f'DNN Validation Loss: {dnn_results_usa}')
+
+    # Get the RSquare
+    dnn_results_usa_rsquare = predict_infections_rsquare(model, test_features, test_labels)
+    print(f'RSquare: {dnn_results_usa_rsquare}')
+
+    # Get predictions of model on test data
+    test_predictions = model.predict(test_features).flatten()
+
+    # Plot model predictions against actual values
+    plot_predictions(test_predictions, test_labels)
+
+
+def train_usa_test_europe():
+    # Load data
+    data = load_data('Project Data', 'europe.csv')
+
+    # Select some features to train and test on
+    features = ['population (discrete data)', 'tests (discrete data)', 'Gini (discrete data)',
+                '%urban pop. (continuous data)', 'actual cases (measured) (discrete data)']
+
+    # Select features and clean data
+    data = preprocess_data(data, features)
+
+    # Split data into train and test samples
+    train_features, train_labels, test_features, test_labels = split_data(data)
+
+    # Define model
+    model = build_and_compile_model(train_features)
+    model.summary()
+
+    # Train model and retrieve performance of model during training
+    model, history = train_model(model, train_features, train_labels)
+
+    # Plot loss during training history
+    plot_loss(history)
+
+    # Evaluate model on test data
+    dnn_results_usa = model.evaluate(test_features, test_labels, verbose=0)
+    print(f'DNN Validation Loss: {dnn_results_usa}')
+
+    # Get the RSquare
+    dnn_results_usa_rsquare = predict_infections_rsquare(model, test_features, test_labels)
+    print(f'RSquare: {dnn_results_usa_rsquare}')
+
+    # Get predictions of model on test data
+    test_predictions = model.predict(test_features).flatten()
+
+    # Plot model predictions against actual values
+    plot_predictions(test_predictions, test_labels)
+
+
+if __name__ == '__main__':
+    # Pipeline that trains a neural network model on USA data and tests on USA data
+    train_usa_test_europe()
+
