9a1aa16f69d4d1e79d0d9a226e47c1f10f38d8d6
Minas Mayth <minasmayth@protonmail.com>
2021-05-18 11:57:09 +0200

    Divided the code in multi_linear_reg.py into more functions for lower cognitive complexity and higher legibility. Have also removed the annoying spacing from the csv files.

--- Multilinear Regression/multi_linear_reg.py
+++ Multilinear Regression/multi_linear_reg.py
@@ -10,7 +10,7 @@ import numpy as np
 from scipy import stats
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import explained_variance_score, mean_absolute_error, r2_score
+from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn import linear_model
 import statsmodels.api as sm
 import seaborn as sns
@@ -19,28 +19,36 @@ US_DATA = r'US States Data.csv'
 EUROPE_DATA = r'europe.csv'
 
 
-def usa_features():
+def usa_features() -> list:
     """
     Features to be used from USA data
-    :return: Relevant column titles of Pandas Dataframes
+    :return: List of column titles of Pandas Dataframes
     """
     return ['Population (discrete data)', 'Tests (discrete data)', 'Gini - gov 2019 (continuous data)',
             '% urban population (continuous data)', 'Actual cases (measured) (discrete data)']
 
 
-def europe_features():
+def europe_features() -> list:
     """
         Features to be used from EU data - This should be the same as the US data
-        :return: Relevant column titles of Pandas Dataframes
+        :return: List of column titles of Pandas Dataframes
         """
-    return ['population (discrete data)', 'tests     (discrete data)', 'Gini      (discrete data)',
-            '%urban pop.  (continuous data)', 'Actual cases']
+    return ['population (discrete data)', 'tests (discrete data)', 'Gini (discrete data)',
+            '%urban pop. (continuous data)', 'Actual cases']
+
+
+def hypothetical1_features() -> list:
+    """
+       Hypothetical Case 1 - tests is equal to population
+       :return: List of column titles of Pandas Dataframes
+       """
+    return ['population (discrete data)', 'tests (discrete data)', 'Gini (discrete data)',
+            '%urban pop. (continuous data)', 'Actual cases']
 
 
 def load_data(folder, filename):
     """
     Loads a csv file from the 'Project Data' directory and returns it as a pandas.Dataframe object.
-
     :param folder: from which folder to load data
     :param filename: .csv file to load
     :return: pandas.Dataframe of corresponding .csv
@@ -50,30 +58,50 @@ def load_data(folder, filename):
 
 
 def snsregressionplot(x, y, title, xlabel, ylabel):
+    """
+    Regression plot using Seaborn library, with information on the graph being returned via
+    the scipy stats library
+    :param x: Array of x values
+    :param y: Array of y values
+    :param title: Plot title
+    :param xlabel: X axis title
+    :param ylabel: Y axis title
+    :return: scipy stats linear regression information for x and y
+    """
     fig = plt.figure()
     ax = sns.regplot(x=x, y=y.astype(float), ci=None, color="b")
     ax.title.set_text(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     plt.show()
-    return(stats.linregress(x,y))
+    return stats.linregress(x, y)
 
 
 def remove_commas(data):
-    # print(type(data))
-    # print(data.shape)
+    """
+    Remove commas from a pandas dataset
+    :param data: Pandas dataframe
+    :return: cleaned dataframe
+    """
     columns = data.columns
     for column in data[columns]:
         for index, value in enumerate(data[column].values):
             if isinstance(value, str):
-                # print(value)
                 data[column].iloc[index] = value.replace(',', '')
     return data
 
 
-def split_data(data):
+def split_data(data, fraction=0.6, index_of_dependent_feature=4):
+    """
+    train_test split data
+    :param data: pandas dataframe
+    :param fraction: what fraction of data to split, default is 0.6 (60%)
+    :param index_of_dependent_feature: index of the dependent feature, which will be assigned to train_labels and
+    test_labels
+    :return: train_features, train_labels, test_features, test_labels - data to be used for building model
+    """
     # Train - Test split
-    train_dataset = data.sample(frac=0.6, random_state=0)
+    train_dataset = data.sample(frac=fraction, random_state=0)
     test_dataset = data.drop(train_dataset.index)
 
     # Look at ranges of features
@@ -83,13 +111,36 @@ def split_data(data):
     train_features = train_dataset.copy()
     test_features = test_dataset.copy()
 
-    train_labels = train_features.pop(4)
-    test_labels = test_features.pop(4)
+    train_labels = train_features.pop(index_of_dependent_feature)
+    test_labels = test_features.pop(index_of_dependent_feature)
 
     return train_features, train_labels, test_features, test_labels
 
 
+def clean_and_convert_data(data, features, filename):
+    """
+    Minor utility function to download the cleaned version of a dataframe with the current features being used
+    :param data: dataset to extract data from
+    :param features: function that returns features to be extracted
+    :param filename: filename to save to
+    :return: excel file
+    """
+    # Select only certain features in the preprocessing pipeline
+    data = data[features]
+
+    # Remove potential commas present in the .csv
+    data = remove_commas(data)
+
+    return data.to_excel(filename + ".xlsx")
+
+
 def preprocess_data(data, features):
+    """
+    Select features, clean data, normalize and scale data
+    :param data: dataset to preprocess
+    :param features: features to select
+    :return: preprocessed dataset
+    """
     # Select only certain features in the preprocessing pipeline
     data = data[features]
 
@@ -110,31 +161,22 @@ def preprocess_data(data, features):
     return data.dropna()
 
 
-def feature_plot(X, Y):
-    fig = plt.figure()
-    ax1 = plt.subplot(2, 2, 1)
-    ax1.scatter(np.array(X)[:, 0], Y)
-    ax1.title.set_text("Population vs Actual cases")
-    ax2 = plt.subplot(2, 2, 2)
-    ax2.scatter(np.array(X)[:, 1], Y)
-    ax2.title.set_text("Tests vs Actual cases")
-    ax3 = plt.subplot(2, 2, 3)
-    ax3.scatter(np.array(X)[:, 2], Y)
-    ax3.title.set_text("Gini vs Actual cases")
-    ax4 = plt.subplot(2, 2, 4)
-    ax4.scatter(np.array(X)[:, 3], Y)
-    ax4.title.set_text("% Urban population vs Actual cases")
-    plt.show()
-
-
-def MultiLinearReg_train_x_test_y(train, test):
+def main_split_and_preprocess(train, test, train_features, test_features):
+    """
+    Main function to preprocess data and split into features and labels
+    :param train: Data to train on
+    :param test: Data to test on
+    :param train_features: features used for training - should contain equivalent information to test_features
+    :param test_features: features used for testing - should contain equivalent information to train_features
+    :return: 
+    """
     # If we are training and testing on the same model, we need to split the data into training and testing samples
     train_test_split = False
     if train == test:
         train_test_split = True
 
     # Helper dict object
-    data_dict = {US_DATA: usa_features(), EUROPE_DATA: europe_features()}
+    data_dict = {US_DATA: train_features, EUROPE_DATA: test_features}
 
     # Load in training data
     train_data = load_data('Project Data', train)
@@ -152,7 +194,6 @@ def MultiLinearReg_train_x_test_y(train, test):
 
         # Select some testing features
         test_features = data_dict[test]
-
         # Select features and clean data
         test_features = preprocess_data(test_data, test_features)
         test_labels = test_features.pop(4)
@@ -161,63 +202,85 @@ def MultiLinearReg_train_x_test_y(train, test):
         train_data = preprocess_data(train_data, train_features)
         train_features, train_labels, test_features, test_labels = split_data(train_data)
 
-    regr = linear_model.LinearRegression().fit(train_features, train_labels)
+    return train_features, train_labels, test_features, test_labels
 
-    print('SKLearn Regression Intercept: \n', regr.intercept_)
-    print('SKlearn Regression Coefficients: \n', regr.coef_)
 
-    importance = regr.coef_
+def SK_build_model(train_features, train_labels):
+    """
+    Build a linear regression model using the SKLearn library and print some information relating to it
+    :param train_features: Independent data to train on
+    :param train_labels: Dependent data to train on
+    :return: SKLearn model object
+    """
+    model = linear_model.LinearRegression().fit(train_features, train_labels)
+
+    print('SKLearn Regression Intercept: \n', model.intercept_)
+    print('SKlearn Regression Coefficients: \n', model.coef_)
+
+    importance = model.coef_
 
     # summarize feature importance
-
     for i, v in enumerate(importance):
         print('SKLearn Feature: %0d, Score: %.5f' % (i, v))
 
+    return model
+
+
+def SM_build_model(train_features, train_labels):
+    """
+    Build a linear regression model using the Statsmodel library and print some information relating to it
+    :param train_features: Independent data to train on
+    :param train_labels: Dependent data to train on
+    :return: SM model object
+    """
     # SM model creation
     model = sm.OLS(train_labels, train_features).fit()
     print_model = model.summary()
     print(print_model)
 
-    predictions1 = regr.predict(train_features)  # SKlearn model predictions
-
-    print(snsregressionplot(predictions1, train_labels, "SKlearn prediction results vs Actual cases (USA)",
-                      "SKLearn prediction results", "Actual Cases (USA)"))
-
-    # print("US data SKlearn model explained_variance_score:", explained_variance_score(train_labels, predictions1))
-    print("US data SKlearn model mean_absolute_error:", mean_absolute_error(train_labels, predictions1))
-    print("US data SKlearn model R^2:", r2_score(train_labels, predictions1))
-
-    predictions2 = model.predict(train_features)  # OLS model predictions
-
-    print(snsregressionplot(predictions2, train_labels, "OLS prediction results vs Actual cases (USA)",
-                      "OLS prediction results", "Actual Cases (USA)"))
-
-    # print("US data OLS model explained_variance_score:", explained_variance_score(train_labels, predictions2))
-    print("US data OLS model mean_absolute_error:", mean_absolute_error(train_labels, predictions2))
-    print("US data OLS model R^2:", r2_score(train_labels, predictions2))
-
-    EUpredictions1 = regr.predict(test_features)
-
-    print(snsregressionplot(EUpredictions1, test_labels, "SKLearn prediction results vs Actual cases (EU)",
-                      "SKLearn prediction results", "Actual Cases (EU)"))
-    # print("EU data SKLearn model explained_variance_score:", explained_variance_score(test_labels, EUpredictions1))
-    print("EU data SKlearn model mean_absolute_error:", mean_absolute_error(test_labels, EUpredictions1))
-    print("EU data SKlearn model R^2:", r2_score(test_labels, EUpredictions1))
-
-    EUpredictions2 = model.predict(test_features)
-
-    print(snsregressionplot(EUpredictions2, test_labels, "OLS prediction results vs Actual cases (EU)",
-                      "OLS prediction results", "Actual Cases (EU)"))
-    # print("EU data OLS model explained_variance_score:", explained_variance_score(test_labels, EUpredictions2))
-    print("EU data OLS model mean_absolute_error:", mean_absolute_error(test_labels, EUpredictions2))
-    print("EU data OLS model R^2:", r2_score(test_labels, EUpredictions2))
+    return model
 
 
-
+def predict_and_metrics(to_predict, actual_values, model, model_type="N/A", dataset="N/A"):
     """
-    Section for testing hypothetical data cases
+    Perform predictions using a built linear regression model and display metrics for it
+    Current metrics supported:
+        -   mean_absolute_error
+        -   R_squared
+    :param to_predict: Unseen data that the model will base its predictions of
+    :param actual_values: Actual recorded values for comparison
+    :param model: Model object to use for predictions
+    :param model_type: string for mentioning which library is being used for the model
+    :param dataset: string for mentioning which dataset is being analysed
+    :return:
     """
+    predictions = model.predict(to_predict)  # SKlearn model predictions
+
+    print(snsregressionplot(predictions, actual_values,
+                            model_type + " Predicted Results vs Actual Results (" + dataset + ")",
+                            xlabel=model_type + " Predicted Results", ylabel="Actual Results (" + dataset + ")"))
+
+    print(dataset + "data " + model_type + " model mean_absolute_error:",
+          mean_absolute_error(actual_values, predictions))
+    print(dataset + " data " + model_type + " model R^2:", r2_score(actual_values, predictions), "\n")
+
+
+def run_main():
+    """
+    Main Function
+    """
+    train_features, train_labels, test_features, test_labels = main_split_and_preprocess(US_DATA, EUROPE_DATA,
+                                                                                         usa_features(),
+                                                                                         europe_features())
+
+    SKlearn_model = SK_build_model(train_features, train_labels)
+    SM_model = SM_build_model(train_features, train_labels)
+
+    predict_and_metrics(train_features, train_labels, SKlearn_model, "SKLearn", "US")
+    predict_and_metrics(train_features, train_labels, SM_model, "Statsmodel", "US")
+    predict_and_metrics(test_features, test_labels, SKlearn_model, "SKLearn", "EU")
+    predict_and_metrics(test_features, test_labels, SM_model, "Statsmodel", "EU")
 
 
 if __name__ == "__main__":
-    MultiLinearReg_train_x_test_y(US_DATA, EUROPE_DATA)
+    run_main()
